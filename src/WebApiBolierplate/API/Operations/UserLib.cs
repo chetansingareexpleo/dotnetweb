9bdcc3ad59d5256f858a5c7e624624c8e5269a74
Chetan Singare <chetan.singare@expleogroup.com>
2023-08-30 11:16:38 +0530

    added new project in rpo

---
+++ src/WebApiBolierplate/API/Operations/UserLib.cs
@@ -0,0 +1,37 @@
+﻿using Core.Constants;
+using API.Models;
+using API.Operations.Interfaces;
+
+namespace API.Operations;
+
+public sealed class UserLib : IUserLib
+{
+    private const string _eddardStark = "Eddard Stark";
+    private readonly ILogger _logger;
+
+    public UserLib(IConfiguration configuration, ILogger<IUserLib> logger)
+    {
+        _logger = logger;
+    }
+
+    public User GetUser(string userName)
+    {
+        try
+        {
+            var user = new User()
+            {
+                Name = userName,
+                Email = $"{userName}@gameofthrones.com",
+                CompanyId = "GOT",
+                Roles = userName == _eddardStark ? Roles.Admin : Roles.User,
+                Id = Guid.NewGuid().ToString("D")
+            };
+            return user;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("GetUser - {message}", ex.Message);
+            return null;
+        }
+    }
+}
---
+++ src/WebApiBolierplate/API/Operations/ValueLib.cs
@@ -0,0 +1,23 @@
+﻿using API.Operations.Interfaces;
+
+namespace API.Operations;
+
+public sealed class ValueLib : IValueLib
+{
+    private string[] _values = { "Jon Snow", "Rob Stark", "Sansa Stark", "Arya Stark", "Bran Stark", "Rickon Stark" };
+
+    public ValueLib(IConfiguration configuration)
+    {
+    }
+
+    public string[] GetValueList() => _values;
+
+    public string GetValueById(int id) => _values.Length <= id ? string.Empty : _values[id];
+
+    public int AddValue(string value) => 1;
+
+    public int UpdateValue(string value, int id) => _values.Length <= id ? -1 : 1;
+
+    public int DeleteValue(int id) => _values.Length <= id ? -1 : 1;
+
+}
